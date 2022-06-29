17fb8fdece997e3ff1767062f96c5845029c642d
Víctor Gómez <vgomez@applaudostudios.com>
2022-06-29 16:31:22 -0400

    Add jwt related functions

--- security/jwt.py
+++ security/jwt.py
@@ -1,5 +1,11 @@
 from datetime import timedelta, datetime
 import jwt
+from jwt import ExpiredSignatureError, InvalidSignatureError
+
+# app imports
+from configuration.enviroment_variables import SECRET_KEY, ALGORITHM
+from data.dataset import users
+from security.authentication import get_index_user_by_username
 
 
 def create_access_token(data: dict, expires_delta: timedelta):
@@ -11,3 +17,20 @@ def create_access_token(data: dict, expires_delta: timedelta):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str):
+    credentials_exception = Exception("Token: Could not validate credentials")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except ExpiredSignatureError:
+        raise credentials_exception
+    except InvalidSignatureError:
+        raise credentials_exception
+    user_index = get_index_user_by_username(username)
+    if user_index == -1:
+        raise credentials_exception
+    return users[user_index]
